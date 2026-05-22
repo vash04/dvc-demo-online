dca5d9c223da4e2991e79879f399a51e0e685d58
sarvesh <joshisarvesh04071996@gmail.com>
2026-05-22 10:49:01 +0530

    add second experiment

--- src/data_ingestion.py
+++ src/data_ingestion.py
@@ -4,8 +4,8 @@ import os
 
 df=pd.read_csv("https://raw.githubusercontent.com/araj2/customer-database/refs/heads/master/Ecommerce%20Customers.csv")
 
-df=df.iloc[:, 3:]
+df=df.iloc[:, 4:]
 
-df=df[df["Length of Membership"]>3]
+df=df[df["Length of Membership"]>2]
 
 df.to_csv(os.path.join('data', 'customer.csv'))
